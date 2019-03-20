76bf7157f6b6899a0aa0c78928f4baddd3684bc0
Axel Pettersson <axel@pettersson.cc>
2019-03-20 15:31:06 +0100

    Added Coffee Button + general app layout

---
+++ src/features/coffeeButton/coffeeButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CoffeeIcon from "./coffeeIcon";
+
+const CoffeeButton = () => (
+  <div>
+    <button className="coffeeButton">
+      <CoffeeIcon />
+    </button>
+  </div>
+);
+
+export default CoffeeButton;
---
+++ src/features/coffeeButton/coffeeIcon.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const coffeIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="100"
+    height="100.778"
+    viewBox="0 0 100 100.778"
+  >
+    <g transform="translate(-144 -356)">
+      <g transform="translate(107 385)">
+        <path
+          d="M91.889,32.778H80.778V16.111H91.889M91.889,5H3V60.556A22.222,22.222,0,0,0,25.222,82.778H58.556A22.222,22.222,0,0,0,80.778,60.556V43.889H91.889A11.111,11.111,0,0,0,103,32.778V16.111A11.108,11.108,0,0,0,91.889,5Z"
+          transform="translate(34 -11)"
+          fill="#fff"
+        />
+      </g>
+      <g data-name="Symbol 1 – 3" transform="translate(-373.154 7)">
+        <rect
+          width="12.02"
+          height="47.154"
+          transform="translate(555.154 349)"
+        />
+        <rect
+          width="12.02"
+          height="47.154"
+          transform="translate(584.307 367) rotate(90)"
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+export default coffeIcon;
