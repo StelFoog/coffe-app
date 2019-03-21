ecf5e86e9613b499db539b46e0adb54b9b890357
samuel <samuel@familjenlarsson.eu>
2019-03-21 18:30:00 +0100

    removed registerServiceWorker at least for now

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Root from "./routes";
-import registerServiceWorker from "./registerServiceWorker";
 import store, { history } from "./store";
-import "typeface-slabo-27px";
 
 ReactDOM.render(
   <Root store={store.store} history={history} persistor={store.persistor} />,
   document.getElementById("root")
 );
-registerServiceWorker();
