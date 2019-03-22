c26bf1a1de85bcd3c7ceaf6ab463f14736a36771
samuel <samuel@familjenlarsson.eu>
2019-03-22 17:17:54 +0100

    added internal db for textbox

---
+++ src/dbi/textboxData.js
@@ -0,0 +1,39 @@
+const data = {
+  noCoffe: {
+    colour: "#3a4",
+    texts: ["Good morning!", "Enjoy your coffe!", "Have a good day!"]
+  },
+  someCoffe: {
+    colour: "#6b4",
+    texts: [
+      "The plant on which coffe beans are grown originates from tropical and southern Africa and tropical Asia.",
+      "The word Latte comes from the Italian word for milk.",
+      'Coffee is known by the scientific community as "the root of all good work."'
+    ]
+  },
+  muchCoffe: {
+    colour: "#9b7",
+    texts: [
+      "You've had quite alot of coffee today haven't you?",
+      "Getting up there...",
+      "Maybe just one more?"
+    ]
+  },
+  enoughCoffe: {
+    colour: "#b87",
+    texts: [
+      "That's all you're gonna have today.",
+      "No more coffe today right?",
+      "It's too much! You're killing your mother! You're killing her!"
+    ]
+  },
+  timesUp: {
+    colour: "#b87",
+    texts: [
+      "Better not drink that if you wanna have a good nights sleep.",
+      "It's getting late, best not to drink another cup."
+    ]
+  }
+};
+
+export default data;
