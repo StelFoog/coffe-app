0bd34e81994b01e47510f045b1f5df646c69c5ea
Axel Pettersson <axel@pettersson.cc>
2019-03-22 23:44:42 +0100

    Added Settings component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import CoffeeButtonContainer from "./features/coffeeButton/CoffeeButtonContainer";
 import StatusContainer from "./features/status/StatusContainer";
 import TextBoxContainer from "./features/textBox/TextBoxContainer";
+import SettingsContainer from "./features/settings/SettingsContainer";
 import "./App.css";
 
 const App = () => (
   <div className="App">
-    <header className="App-header">Settings will go here</header>
+    <header className="App-header">
+      <SettingsContainer />
+    </header>
     <TextBoxContainer />
     <CoffeeButtonContainer />
     <StatusContainer />
---
+++ src/features/settings/Settings.js
@@ -0,0 +1,7 @@
+import React from "react";
+import SettingsIcon from "./SettingsIcon";
+import "./settings.css";
+
+const Settings = () => <SettingsIcon />;
+
+export default Settings;
