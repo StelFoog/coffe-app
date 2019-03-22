16009c9029848a9ba12775b8d51b115744f0b0fb
Axel Pettersson <axel@pettersson.cc>
2019-03-23 00:11:05 +0100

    Settings icon styling

--- src/features/settingsButton/SettingsButton.js
+++ src/features/settingsButton/SettingsButton.js
@@ -2,6 +2,10 @@ import React from "react";
 import SettingsIcon from "./SettingsButtonIcon";
 import "./settingsButton.css";
 
-const Settings = () => <SettingsIcon />;
+const Settings = () => (
+  <button className="settings-button" onClick={() => {}}>
+    <SettingsIcon />
+  </button>
+);
 
 export default Settings;
